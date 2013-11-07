e24d8672222a68e0d3a2ce5a626fd45cf98fccfd
Dmitry Chornyi <dima.chornyi@gmail.com>
2013-11-06 22:22:32 -0800

    Week 7 homework

---
+++ scratchpad/.worksheet/src/week7.ws.scala
@@ -0,0 +1,5 @@
+package week7
+
+object ws {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(48); val res$0 = 
+  "blah".indexOf('x');System.out.println("""res0: Int = """ + $show(res$0))}
+}
---
+++ scratchpad/src/week7/ws.sc
@@ -0,0 +1,5 @@
+package week7
+
+object ws {
+  "blah".indexOf('x')                             //> res0: Int = -1
+}
